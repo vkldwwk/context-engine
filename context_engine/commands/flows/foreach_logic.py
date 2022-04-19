1b46fd3993c382c3d95768c73a0f23568029f6c1
vkldwwk <jeff.elliott.dev@gmail.com>
2022-04-19 12:28:24 -0400

    fixing issue with for vars

--- context_engine/commands/flows/foreach_logic.py
+++ context_engine/commands/flows/foreach_logic.py
@@ -1,8 +1,14 @@
-from ...decorators import default_var_when_none
+from ...decorators import get_composite_key_value
 
 def foreach_logic(engine,flow_step):
-    if flow_step.is_var_list:
-        for x,y in enumerate(engine.context[flow_step.collection]):
+    collection = get_composite_key_value(engine.context,flow_step.collection)
+    if flow_step.is_var_list and type(collection) is dict:
+        for x,y in collection.items():
+            engine.set_local(flow_step.var[0],x)
+            engine.set_local(flow_step.var[1],y)
+            engine.do_steps(flow_step.steps)
+    elif flow_step.is_var_list:
+        for x,y in enumerate(collection):
             engine.set_local(flow_step.var[0],x)
             engine.set_local(flow_step.var[1],y)
             engine.do_steps(flow_step.steps)
